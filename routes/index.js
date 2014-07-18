102b815db5c0fe9692fff743d33f52f74ef48e0f
Nick Alekhine <nalekhine@aereo.com>
2014-07-18 13:32:34 -0400

    add in rebound page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function( req, res ) {
   res.render('index', { title: 'Interactions' });
 });
 
+router.get('/rebound', function( req, res ) {
+  res.render('rebound', { title: 'Using Rebound' });
+});
+
 module.exports = router;

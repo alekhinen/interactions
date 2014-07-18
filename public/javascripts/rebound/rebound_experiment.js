ebf0ae82b8f9ce1aa2177cfb0e632a37b3d1971c
Nick Alekhine <nalekhine@aereo.com>
2014-07-18 14:19:31 -0400

    more experimentation with reboundjs.

--- public/javascripts/rebound/rebound_experiment.js
+++ public/javascripts/rebound/rebound_experiment.js
@@ -1,33 +1,34 @@
+// Test 1 ---------------------------------------------------------------------
 // Get a reference to the logo element.
-var el = document.getElementById('test1_btn');
+var test1 = document.getElementById('test1_btn');
 
 // create a SpringSystem and a Spring with a bouncy config.
 var springSystem = new rebound.SpringSystem();
-var spring = springSystem.createSpring(50, 3);
+var t1Spring = springSystem.createSpring(50, 3);
 
 // Add a listener to the spring. Every time the physics
 // solver updates the Spring's value onSpringUpdate will
 // be called.
-spring.addListener({
+t1Spring.addListener({
   onSpringUpdate: function(spring) {
-    var val = spring.getCurrentValue();
+    var val = t1Spring.getCurrentValue();
     val = rebound.MathUtil.mapValueInRange(val, 0, 1, 1, 0.5);
-    scale(el, val);
+    scale(test1, val);
   }
 });
 
 // Listen for mouse down/up/out and toggle the
 //springs endValue from 0 to 1.
-el.addEventListener('mousedown', function() {
-  spring.setEndValue(1);
+test1.addEventListener('mousedown', function() {
+  t1Spring.setEndValue(1);
 });
 
-el.addEventListener('mouseout', function() {
-  spring.setEndValue(0);
+test1.addEventListener('mouseout', function() {
+  t1Spring.setEndValue(0);
 });
 
-el.addEventListener('mouseup', function() {
-  spring.setEndValue(0);
+test1.addEventListener('mouseup', function() {
+  t1Spring.setEndValue(0);
 });
 
 // Helper for scaling an element with css transforms.
@@ -38,3 +39,32 @@ function scale(el, val) {
   el.style.transform = 'scale3d(' +
     val + ', ' + val + ', 1)';
 }
+
+
+
+// Test 2 ---------------------------------------------------------------------
+// Get a ref to the image
+var test2 = document.getElementById('test2_img');
+var t2Clicked = false;
+
+// SpringSystem for the image
+var t2Spring = springSystem.createSpring(50, 6);
+
+t2Spring.addListener({
+  onSpringUpdate: function( spring ) {
+    var val = t2Spring.getCurrentValue();
+    val = rebound.MathUtil.mapValueInRange(val, 0, 1, 1, 0.5);
+    scale( test2, val );
+  }
+});
+
+// Image event listeners.
+test2.addEventListener('click', function() {
+  if ( t2Clicked ) {
+    t2Spring.setEndValue(0);
+    t2Clicked = false;
+  } else {
+    t2Spring.setEndValue(-3);
+    t2Clicked = true;
+  }
+});

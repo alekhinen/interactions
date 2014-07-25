9de63257558a57bc86968d32b31b0d56efe32cae
Nick Alekhine <nalekhine@aereo.com>
2014-07-25 15:49:24 -0400

    more d3 experiments.

--- public/javascripts/d3/experiment.js
+++ public/javascripts/d3/experiment.js
@@ -1,7 +1,8 @@
 // data
 var circleRadii = [40, 20, 10];
 
-// setting stuff for dom.
+// ----------------------------------------------------------------------------
+// circles inside circles
 var bodySelection = d3.select('body');
 
 var svgSelection = bodySelection.append('svg')
@@ -28,3 +29,46 @@ var circleAttributes = circles
       return 'red';
     }
   } );
+
+// ----------------------------------------------------------------------------
+// handling json
+var jsonCircles = [
+  {
+    'x_axis': 30,
+    'y_axis': 30,
+    'radius': 20,
+    'color': 'green'
+  }, {
+    'x_axis': 70,
+    'y_axis': 70,
+    'radius': 20,
+    'color': 'purple'
+  }, {
+    'x_axis': 110,
+    'y_axis': 100,
+    'radius': 20,
+    'color': 'red'
+  }];
+
+var svgContainer = bodySelection.append( 'svg' )
+  .attr( 'width', 200 )
+  .attr( 'height', 200 );
+
+var jCircles = svgContainer.selectAll( 'circle' )
+  .data( jsonCircles )
+  .enter()
+  .append( 'circle' );
+
+var jCirAttr = jCircles
+  .attr( 'cx', function ( d ) {
+    return d.x_axis;
+  })
+  .attr( 'cy', function ( d ) {
+    return d.y_axis;
+  })
+  .attr( 'r', function ( d ) {
+    return d.radius;
+  })
+  .style( 'fill', function ( d ) {
+    return d.color;
+  });
